4514bf080b2a733017a5fb6d5415c93ea9ff293d
ligy <ligy@zetyun.com>
2023-08-25 10:36:23 +0800

    父项目同步

--- src/main/java/com/ligy/graduation/GraduationApplication.java
+++ src/main/java/com/ligy/graduation/GraduationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GraduationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GraduationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GraduationApplication.class, args);
+    }
 
 }
